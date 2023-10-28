2e693b3e2baaaa0de3c092b634605b46c55efd44
vmancode <146589771+vmancode@users.noreply.github.com>
2023-10-28 21:31:24 +0000

    commit2

--- app.rb
+++ app.rb
@@ -15,9 +15,34 @@ end
 
 
 get ("/square/results") do
-
   @num1 = params.fetch("number1").to_f
   @num1result = params.fetch("number1").to_f ** 2
-
 erb(:new_square_results)
   end
+
+
+  get ("/square_root/new") do
+    erb(:new_sqrt_calc)
+    end
+    
+    get ("/square_root/results") do
+      @num2 = params.fetch("number2").to_f
+      @num2result = params.fetch("number2").to_f ** 0.5
+    erb(:new_sqrt_results)
+      end
+
+
+
+
+
+get ("/payment/new") do
+        erb(:payment_new)
+        end
+        
+get ("/payment/results") do
+          @num3 = params.fetch("num3").to_f
+          @num4 = params.fetch("num4").to_f
+          @num5 = params.fetch("num5").to_f
+          @num5result = params.fetch("num3").to_f 
+        erb(:payment_results)
+          end
