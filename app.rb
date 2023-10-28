df2684f842e98e9e05bccc252546797d466ba4fc
vmancode <146589771+vmancode@users.noreply.github.com>
2023-10-28 21:01:28 +0000

    commit1

--- app.rb
+++ app.rb
@@ -6,4 +6,18 @@ get("/") do
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+erb(:new_square_calc)
 end
+
+get ("/square/new") do
+erb(:new_square_calc)
+end
+
+
+get ("/square/results") do
+
+  @num1 = params.fetch("number1").to_f
+  @num1result = params.fetch("number1").to_f ** 2
+
+erb(:new_square_results)
+  end
